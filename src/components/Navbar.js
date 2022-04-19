beda2d316b999c20c155ea786dd77945e869e424
ejioguvictor <ejioguvictor6054@gmail.com>
2022-04-19 23:58:38 +0100

    started foodhouse app

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <div className='left-nav'></div>
+      <div className='right-nav'></div>
+
+    </div>
+  )
+}
+
+export default Navbar
