e5142f3c6b4d28b0646b58d2d65c373bffabc1b0
ejioguvictor <ejioguvictor6054@gmail.com>
2022-04-20 15:20:39 +0100

    added modified menuItem component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from './components/NavBar/NavBar';
 import Home from './pages/Home/Home'
 import Footer from './components/Footer/Footer'
+import Menu from './pages/Menu/Menu'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 
@@ -12,6 +13,7 @@ function App() {
         <NavBar />
         <Routes>
           <Route exact path='/' element={<Home />} />
+          <Route exact path='/menu' element={<Menu />} />
         </Routes>
         <Footer />
       </Router>
--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { MenuList } from '../../helpers/MenuLists'
 import styles from './Menu.module.scss'
+
 
 const Menu = () => {
   return (
     <div className={styles.menu}>
       <h1 className={styles.menuTitle}>Our Menu</h1>
       <div className={styles.menuList}>
-
+        {MenuList.map((menuItem, key) => {
+          return <div>{menuItem.name}</div>
+          // <div>{menuItem.image}</div>
+          // <div>{menuItem.price}</div>
+        })}
       </div>
     </div>
   )
