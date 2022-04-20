08b30f6e5828c03985dc7a7443bbf8996e4377b4
ejioguvictor <ejioguvictor6054@gmail.com>
2022-04-20 15:34:23 +0100

    modified files

--- src/components/MenuItems/MenuItem.js
+++ src/components/MenuItems/MenuItem.js
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from './MenuItem.module.scss'
 
 function MenuItem({ image, name, price }) {
   return (
-    <div className="menuItem">
+    <div className={styles.menuItem}>
       <div style={{ backgroundImage: `url(${image})` }}> </div>
       <h1> {name} </h1>
       <p> ${price} </p>
--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { MenuList } from '../../helpers/MenuLists'
+import MenuItem from '../../components/MenuItems/MenuItem'
 import styles from './Menu.module.scss'
 
 
@@ -9,9 +10,10 @@ const Menu = () => {
       <h1 className={styles.menuTitle}>Our Menu</h1>
       <div className={styles.menuList}>
         {MenuList.map((menuItem, key) => {
-          return <div>{menuItem.name}</div>
-          // <div>{menuItem.image}</div>
-          // <div>{menuItem.price}</div>
+          return <MenuItem key={key}
+            image={menuItem.image}
+            name={menuItem.name}
+            price={menuItem.price} />
         })}
       </div>
     </div>
