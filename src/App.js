37b8104d297b7fb75aafa5394861a6b0589bd9b4
ejioguvictor <ejioguvictor6054@gmail.com>
2022-04-20 08:26:57 +0100

    mapped out app outline

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import NavBar from './components/NavBar';
+import Home from './pages/Home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+
 function App() {
   return (
     <div className="App">
-      <h1>violaaa</h1>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Logo from "../assets/pizzaLogo.png"
+// import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='nav'>
-      <div className='left-nav'></div>
+    <div className='navbar'>
+      <div className='left-nav'>
+        <img src={Logo} alt='pizza logo' />
+      </div>
       <div className='right-nav'></div>
 
     </div>
