ae03bf6718e381de4ee88f223d6ad27acc61968a
ejioguvictor <ejioguvictor6054@gmail.com>
2022-04-20 09:50:00 +0100

    made changes to the styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import NavBar from './components/NavBar/NavBar';
-import Home from './pages/Home'
+import Home from './pages/Home/Home'
 // import Footer from './components/Footer'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
